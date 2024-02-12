e4fecd4ca9fff2e8aa201b9a0f70ef0bce6897e8
AdrianaP-MM <136763124+AdrianaP-MM@users.noreply.github.com>
2024-02-11 18:16:29 -0600

    NavBar Cambios

--- controlador/utilidades(plantillas)/publico.js
+++ controlador/utilidades(plantillas)/publico.js
@@ -15,25 +15,7 @@ document.querySelector('title').textContent = 'FeasVerse';
 const loadTemplate = async () => {
     // *Se agrega el encabezado de la página web antes del contenido principal.
     MAIN.insertAdjacentHTML('beforebegin', `
-        <header  >
-            <nav class="navbar navbar-expand-lg fixed-top" id="header" data-bs-theme="dark">
-                <div class="container">
-                    <a class="navbar-brand" href="index.html">
-                        <img src="../../recursos/imagenes/logo.svg" height="70" alt="FeasVerse">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav ms-auto">
-                            <a class="nav-link" href="index.html"><i class="bi bi-shop"></i> Catálogo</a>
-                            <a class="nav-link" href="cart.html"><i class="bi bi-cart"></i> Carrito</a>
-                            <a class="nav-link" href="#" onclick="logOut()"><i class="bi bi-box-arrow-left"></i> Cerrar sesión</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
+    
     `);
     //* Se agrega el pie de la página web después del contenido principal.
     MAIN.insertAdjacentHTML('afterend', `
