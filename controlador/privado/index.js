0d0e6a62c74e32016f00f7271aa98768d0146200
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-02-15 19:01:56 -0600

    Creación del primer paso de olvidaste tu contraseña donde se le pide el correo electronico

--- controlador/privado/index.js
+++ controlador/privado/index.js
@@ -1,11 +1,21 @@
 // Constante para establecer el formulario de inicio de sesión.
 const LOGIN_FORM = document.getElementById('loginForm');
+const FORGOT_PASSWORD_FORM = document.getElementById('forgetpasswordstepone');
 
 document.addEventListener('DOMContentLoaded', async () => {
-    
     // Se muestra el formulario para iniciar sesión.
     LOGIN_FORM.classList.remove('d-none');
-
+    // Se oculta el formulario de recuperación de contraseña.
+    FORGOT_PASSWORD_FORM.classList.add('d-none');
     // Se establece el título del contenido principal.
     MAIN_TITLE.textContent = 'FEASVERSE - Inicio de sesión';
 });
+
+function showForgotPasswordForm() {
+    // Se oculta el formulario para iniciar sesión.
+    LOGIN_FORM.classList.add('d-none');
+    // Se muestra el formulario de recuperación de contraseña.
+    FORGOT_PASSWORD_FORM.classList.remove('d-none');
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'FEASVERSE - Recuperar contraseña';
+}
