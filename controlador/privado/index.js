914f0afed9e6542e38387e9e5726371ff50dc69f
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-02-18 13:53:58 -0600

    Cambios realizados del login

--- controlador/privado/index.js
+++ controlador/privado/index.js
@@ -1,3 +1,9 @@
+//* Constante para establecer el elemento del contenido principal.
+const MAIN = document.querySelector('main');
+// Se establece el título de la página web.
+document.querySelector('title').textContent = 'FeasVerse - Admin';
+const MAIN_TITLE = document.getElementById('mainTitle');
+
 // Constante para establecer el formulario de inicio de sesión, olvidaste contraseña paso 1,2 y 3.
 const LOGIN_FORM = document.getElementById('loginForm');
 const FORGOT_PASSWORD_FORM = document.getElementById('forgetpasswordstepone');
@@ -7,22 +13,22 @@ const FORGOT_PASSWORD_STEP_THREE_FORM = document.getElementById('forgetpasswords
 document.addEventListener('DOMContentLoaded', async () => {
     // Se muestra el formulario para iniciar sesión.
     LOGIN_FORM.classList.remove('d-none');
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'FEASVERSE - Inicio de sesión';
     // Se oculta el formulario de restablecimiento de contraseña (paso 1 y 2).
     FORGOT_PASSWORD_FORM.classList.add('d-none');
     FORGOT_PASSWORD_STEP_TWO_FORM.classList.add('d-none');
-    // Se establece el título del contenido principal.
-    MAIN_TITLE.textContent = 'FEASVERSE - Inicio de sesión';
 });
 
 function showLoginForm() {
     // Se muestra el formulario para iniciar sesión.
     LOGIN_FORM.classList.remove('d-none');
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'FEASVERSE - Inicio de sesión';
     // Se oculta el formulario de restablecimiento de contraseña (paso 1 y 2 y 3).
     FORGOT_PASSWORD_FORM.classList.add('d-none');
     FORGOT_PASSWORD_STEP_TWO_FORM.classList.add('d-none');
     FORGOT_PASSWORD_STEP_THREE_FORM.classList.add('d-none');
-    // Se establece el título del contenido principal.
-    MAIN_TITLE.textContent = 'FEASVERSE - Inicio de sesión';
 }
 
 function showForgotPasswordForm() {
