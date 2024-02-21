6465f60c77ebc566e71374c792304bc091deed68
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-02-20 21:24:41 -0600

    primer diseño de tabla de cliente preliminar y comienzo de detalles cliente

--- controlador/privado/cliente.js
+++ controlador/privado/cliente.js
@@ -1,8 +1,13 @@
 // Se establece el título de la página web.
 document.querySelector('title').textContent = 'Feasverse - Clientes';
 
+// Constante para establecer la tabla
+const TABLE_CLIENT = document.getElementById('tabla_cliente');
+
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     // *Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
+    // Se muestra la tabla.
+    //TABLE_CLIENT.classList.remove('d-none');
 });
