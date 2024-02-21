73894b112cc17612b88cd9573b9aa2214d264c32
AdrianaP-MM <136763124+AdrianaP-MM@users.noreply.github.com>
2024-02-20 22:33:42 -0600

    Trabajador

--- controlador/privado/trabajadores.js
+++ controlador/privado/trabajadores.js
@@ -2,4 +2,22 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // *Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
+
+    var primeraPestana = document.querySelector('#tabla-tab');
+    if (primeraPestana) {
+        primeraPestana.click();
+    }
+
 });
+
+function cambiarColor(boton) {
+    // Restablece el color de todos los botones
+    var botones = document.querySelectorAll('.boton-cambiar-color');
+    botones.forEach(function (b) {
+        b.style.backgroundColor = '#146A93';
+    });
+
+    // Cambia el color del botón clicado
+    boton.style.backgroundColor = '#1591CC';
+    boton.style.color = 'white';
+}
