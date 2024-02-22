cb98b1b4d84c40d7029fa268385f25225081b38b
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-02-22 11:23:02 -0600

    Arreglos

--- controlador/privado/cliente.js
+++ controlador/privado/cliente.js
@@ -6,22 +6,13 @@ document.addEventListener('DOMContentLoaded', async () => {
     // *Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
 
-    // Manejador de eventos para el doble clic en la tabla
-    document.getElementById('tableBody').addEventListener('dblclick', function (event) {
-        // Verificar si el doble clic fue en una fila
-        const targetRow = event.target.closest('tr');
-        if (targetRow) {
-            // Aquí puedes obtener los datos de la fila y mostrar el modal
-            const rowData = Array.from(targetRow.cells).map(cell => cell.textContent);
-            showDetailModal(rowData);
-        }
-    });
+   
 
     // Bloquear el botón y mostrar la pestaña al cargar la página
     // Mostrar la pestaña
     document.getElementById('tabla-tab').click();
     // Bloquear el botón
-    document.getElementById('tabla-tab').setAttribute('disabled', 'disabled');
+    document.getElementById('tabla-tab').setAttribute('disabled', 'disabled'); 
 });
 
 // Función para mostrar el modal con los detalles de la fila
@@ -30,8 +21,10 @@ function showDetailModal(rowData) {
     const detailUpdateForm = document.getElementById('detailUpdateForm');
 
     modalTitle.textContent = 'Detalles del Cliente';
-    //detailUpdateForm.querySelector("#validationCustomName").value = rowData[2]; // Nombre
-    //detailUpdateForm.querySelector("#validationCustomLastName").value = rowData[1]; // Apellidos
+    var id, ape, nom, dui, tel, eml, nvl;
+
+    detailUpdateForm.querySelector("#validationCustomName").value = nom; // Nombre
+    detailUpdateForm.querySelector("#validationCustomLastName").value = ape; // Apellidos
 
     // Mostrar el modal
     const detailModal = new bootstrap.Modal(document.getElementById('detailModal'));
