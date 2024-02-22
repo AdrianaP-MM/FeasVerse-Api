2353d9337d5b1ee8ed0e5e6c8327b75fcff8f439
Fernandouu <20190369@ricaldone.edu.sv>
2024-02-22 14:45:56 -0600

    Pagina Comentario

---
+++ controlador/privado/comentario.js
@@ -0,0 +1,8 @@
+// Se establece el título de la página web.
+document.querySelector('title').textContent = 'Feasverse - Comentarios';
+
+// *Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', async () => {
+    // *Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+});
