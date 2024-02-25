7b6f1559e67d0fbdf7d3448c70e7f735ba5fc842
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-02-24 22:31:58 -0600

    Cambio de color al modal del cliente y continuacion de diseño de pedidos, donde ya se hizo el diseño de pedidos y parte del diseño de las cards

--- controlador/privado/pedidos.js
+++ controlador/privado/pedidos.js
@@ -1,8 +1,55 @@
 // Se establece el título de la página web.
 document.querySelector('title').textContent = 'Feasverse - Pedidos';
 
+// Constante para establecer el espacio de tabla y el espacio de agregar.
+const PEDIDOS_DIV = document.getElementById('pedidos');
+const REPARTIDOR_DIV = document.getElementById('repartidor');
+
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     // *Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
+
+    var primeraPestana = document.querySelector('#pedidos-tab');
+    if (primeraPestana) {
+        primeraPestana.click();
+        // Se muestra el div de tabla
+        PEDIDOS_DIV.classList.remove('d-none');
+        REPARTIDOR_DIV.classList.add('d-none');
+    }
 });
+
+function ShowRepartidor(boton) {
+    // Se muestra el div para agregar trabajador.
+    REPARTIDOR_DIV.classList.remove('d-none');
+    // Se oculta el formulario de tabla.
+    PEDIDOS_DIV.classList.add('d-none');
+
+    // Restablece el color de todos los botones
+    var botones = document.querySelectorAll('.boton-cambiar-color');
+    botones.forEach(function (b) {
+        b.style.backgroundColor = '#146A93';
+    });
+
+    // Cambia el color del botón clicado
+    boton.style.backgroundColor = '#1A89BD';
+    boton.style.color = 'white';
+}
+
+
+function ShowPedidos(boton) {
+    // Se muestra el div para agregar trabajador.
+    PEDIDOS_DIV.classList.remove('d-none');
+    // Se oculta el formulario de tabla.
+    REPARTIDOR_DIV.classList.add('d-none');
+
+    // Restablece el color de todos los botones
+    var botones = document.querySelectorAll('.boton-cambiar-color');
+    botones.forEach(function (b) {
+        b.style.backgroundColor = '#146A93';
+    });
+
+    // Cambia el color del botón clicado
+    boton.style.backgroundColor = '#1A89BD';
+    boton.style.color = 'white';
+}
