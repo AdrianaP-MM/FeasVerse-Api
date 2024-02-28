ea53aaed15f33d029d6dc9323632ef0f17b18d2a
AdrianaP-MM <136763124+AdrianaP-MM@users.noreply.github.com>
2024-02-27 23:16:46 -0600

    Marcas Finalizado

--- controlador/privado/marcas.js
+++ controlador/privado/marcas.js
@@ -1,9 +1,24 @@
+const NOMBRE_INPUT = document.getElementById('nombreMarca'),
+    DESC_INPUT = document.getElementById('descripcionMarca'),
+    IMG_INPUT = document.getElementById('selectedImage');
+
+const NOMBRED_INPUT = document.getElementById('nombreMarcaD'),
+    DESCD_INPUT = document.getElementById('descripcionMarcaD'),
+    IMGD_INPUT = document.getElementById('selectedImageD');
+
+const DATA_MODAL = new bootstrap.Modal('#dataModal'),
+    MODAL_TITLE = document.getElementById('modalTitle'),
+    UPDATE_FORM = document.getElementById('updateFrom');
+
+var primeraPestana = document.querySelector('#marcas-tab');
+
+const BOTON_ACTUALIZAR = document.getElementById('actualizarBtn');
+
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     // *Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
 
-    var primeraPestana = document.querySelector('#marcas-tab');
     if (primeraPestana) {
         primeraPestana.click();
         // Se muestra el div de tabla
@@ -15,7 +30,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 // Constante para establecer el espacio de tabla y el espacio de agregar.
 const MARCA_DIV = document.getElementById('marcas');
 const ADD_DIV = document.getElementById('agregar');
-
 
 function showAddDiv(boton) {
     // Se muestra el div para agregar trabajador.
@@ -59,7 +73,7 @@ function displaySelectedImage(event, elementId) {
     if (fileInput.files && fileInput.files[0]) {
         const reader = new FileReader();
 
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             selectedImage.src = e.target.result;
         };
 
@@ -67,22 +81,79 @@ function displaySelectedImage(event, elementId) {
     }
 }
 
+DATA_MODAL._element.addEventListener('hidden.bs.modal', function () {
+    // Después de que el modal se haya ocultado, cambiar el texto del botón a "Actualizar"
+    BOTON_ACTUALIZAR.textContent = "Actualizar";
+});
+
+const returnBack = async () => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Seguro qué quieres cancelar?', 'Los datos ingresados no serán guardados');
+    if (RESPONSE.isConfirmed) {
+        NOMBRE_INPUT.value = ' ';
+        DESC_INPUT.value = ' ';
+        IMG_INPUT.src = 'https://mdbootstrap.com/img/Photos/Others/placeholder.jpg';
+        primeraPestana.click();
+        // Se muestra el div de tabla
+        MARCA_DIV.classList.remove('d-none');
+        ADD_DIV.classList.add('d-none');
+    }
+}
+
+const addSave = async () => {
+    await sweetAlert(1, 'Se ha guardado correctamente', true);
+    NOMBRE_INPUT.value = ' ';
+    DESC_INPUT.value = ' ';
+    IMG_INPUT.src = 'https://mdbootstrap.com/img/Photos/Others/placeholder.jpg';
+}
+
 const openDetails = async () => {
-
-    const NOMBRE_MARCA_INPUT = document.getElementById('nombreMarcaD'),
-        DESCRIPCION_MARCA_INPUT = document.getElementById('descripcionMarcaD');
-
-    const DATA_MODAL = new bootstrap.Modal('#dataModal'),
-        MODAL_TITLE = document.getElementById('modalTitle'),
-        UPDATE_FORM = document.getElementById('updateFrom');
-
     // Se muestra la caja de diálogo con su título.
     DATA_MODAL.show();
     // Se prepara el formulario.
     UPDATE_FORM.reset();
     // Ahora puedes hacer lo que necesites con el array de valores
-    NOMBRE_MARCA_INPUT.value = "ADIDAS";
-    DESCRIPCION_MARCA_INPUT.value = 'Buena marca de zapatos';
+    NOMBRED_INPUT.value = "ADIDAS";
+    DESCD_INPUT.value = 'Buena marca de zapatos';
+
+    IMGD_INPUT.src = '/recursos/imagenes/marcas/adidas.svg';
     MODAL_TITLE.textContent = 'Detalles Marca';
+}
+
+
+const botonActualizar = async () => {
+    var textoBoton = BOTON_ACTUALIZAR.textContent.trim();
+
+    if (textoBoton == 'Actualizar') {
+
+        NOMBRED_INPUT.readOnly = false;
+        DESCD_INPUT.readOnly = false;
+
+        BOTON_ACTUALIZAR.textContent = "Guardar";
+    }
+    else if (textoBoton == 'Guardar') {
+
+        await sweetAlert(1, 'Se ha actualizado correctamente', true);
+        DATA_MODAL.hide();
+    }
+}
+
+const botonCancelar = async () => {
+    var textoBoton = BOTON_ACTUALIZAR.textContent.trim();
+
+    if (textoBoton == 'Actualizar') {
+        // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+        const RESPONSE = await confirmAction('¿Seguro qué quieres regresar?', 'Se cerrará la ventana emergente');
+        if (RESPONSE.isConfirmed) {
+            DATA_MODAL.hide();
+        }
+    }
+    else if (textoBoton == 'Guardar') {
+        // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+        const RESPONSE = await confirmAction('¿Seguro qué quieres regresar?', 'Si has modificado no se guardará');
+        if (RESPONSE.isConfirmed) {
+            DATA_MODAL.hide();
+        }
+    }
 
 }
