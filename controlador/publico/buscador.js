84eb002cb522083bae749709e7ff6fcad98254e9
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-03-08 21:46:42 -0600

    Trabajo del filtro del buscador

--- controlador/publico/buscador.js
+++ controlador/publico/buscador.js
@@ -3,3 +3,29 @@ document.addEventListener('DOMContentLoaded', async () => {
     // *Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
 });
+
+// Función para mostrar/ocultar el icono de limpiar según si hay texto en el input
+document.getElementById('buscadorInputZapatos').addEventListener('input', function () {
+    var searchIcon = document.querySelector('.search-icon');
+    var clearIcon = document.querySelector('.clear-icon');
+
+    if (this.value.length > 0) {
+        clearIcon.style.display = 'block';
+    } else {
+        searchIcon.style.display = 'block';
+        clearIcon.style.display = 'none';
+    }
+});
+
+// Función para limpiar el input y ocultar el icono de limpiar
+function clearSearch() {
+    var input = document.getElementById('buscadorInputZapatos');
+    var searchIcon = document.querySelector('.search-icon');
+    var clearIcon = document.querySelector('.clear-icon');
+
+    input.value = '';
+    input.focus();
+
+    searchIcon.style.display = 'block';
+    clearIcon.style.display = 'none';
+}
