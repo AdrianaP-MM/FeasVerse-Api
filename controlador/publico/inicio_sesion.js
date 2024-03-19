cf2de4e5e73dade68714e13066cf4f98cdaeee92
Fernandouu <20190369@ricaldone.edu.sv>
2024-03-19 13:52:58 -0600

    Formularios de Recuperacion de contraseña

--- controlador/publico/inicio_sesion.js
+++ controlador/publico/inicio_sesion.js
@@ -1,9 +1,9 @@
 // Constante para establecer el formulario de inicio de sesión, olvidaste contraseña paso 1,2 y 3.
 const MAIN_TITLE = document.getElementById('mainTitle');
 const LOGIN_FORM = document.getElementById('loginForm');
-const FORGOT_PASSWORD_FORM = document.getElementById('password1');
-const FORGOT_PASSWORD_STEP_TWO_FORM = document.getElementById('password2');
-const FORGOT_PASSWORD_STEP_THREE_FORM = document.getElementById('password3');
+const PASSWORD1_FORM = document.getElementById('password1');
+const PASSWORD2_FORM = document.getElementById('password2');
+const PASSWORD3_FORM = document.getElementById('password3');
 const REGISTRO_FORM = document.getElementById('registro');
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -17,41 +17,55 @@ document.addEventListener('DOMContentLoaded', async () => {
 function Registro() {
     // Se oculta el formulario para iniciar sesión y paso 2, 3.
     LOGIN_FORM.classList.add('d-none');
+    PASSWORD1_FORM.classList.add('d-none');
+    PASSWORD2_FORM.classList.add('d-none');
+    PASSWORD3_FORM.classList.add('d-none');
     // Se muestra el formulario de restablecimiento de contraseña.
     REGISTRO_FORM.classList.remove('d-none');
     // Se establece el título del contenido principal.
     MAIN_TITLE.textContent = 'FEASVERSE - Registro de Usuario';
 }
 
-function showForgotPasswordForm() {
+function showPass1() {
     // Se oculta el formulario para iniciar sesión y paso 2, 3.
     LOGIN_FORM.classList.add('d-none');
-    FORGOT_PASSWORD_STEP_TWO_FORM.classList.add('d-none');
-    FORGOT_PASSWORD_STEP_THREE_FORM.classList.add('d-none');
+    PASSWORD1_FORM.classList.remove('d-none');
+    PASSWORD2_FORM.classList.add('d-none');
+    PASSWORD3_FORM.classList.add('d-none');
+    REGISTRO_FORM.classList.add('d-none');
     // Se muestra el formulario de restablecimiento de contraseña.
-    FORGOT_PASSWORD_FORM.classList.remove('d-none');
     // Se establece el título del contenido principal.
     MAIN_TITLE.textContent = 'FEASVERSE - Recuperar contraseña';
 }
 
-function showForgotPasswordStepTwoForm() {
+function showPass2() {
     // Se oculta el formulario para iniciar sesión y paso 1, 3
     LOGIN_FORM.classList.add('d-none');
-    FORGOT_PASSWORD_FORM.classList.add('d-none');
-    FORGOT_PASSWORD_STEP_THREE_FORM.classList.add('d-none');
-    // Se muestra el formulario de recuperación de contraseña (paso 2).
-    FORGOT_PASSWORD_STEP_TWO_FORM.classList.remove('d-none');
+    PASSWORD1_FORM.classList.add('d-none');
+    PASSWORD2_FORM.classList.remove('d-none');
+    PASSWORD3_FORM.classList.add('d-none');
+    REGISTRO_FORM.classList.add('d-none');
     // Se establece el título del contenido principal.
     MAIN_TITLE.textContent = 'FEASVERSE - Recuperar contraseña';
 }
 
-function showForgotPasswordStepThreeForm() {
+function showPass3() {
     // Se oculta el formulario para iniciar sesión y paso 1, 2
     LOGIN_FORM.classList.add('d-none');
-    FORGOT_PASSWORD_FORM.classList.add('d-none');
-    FORGOT_PASSWORD_STEP_TWO_FORM.classList.add('d-none');
+    PASSWORD1_FORM.classList.add('d-none');
+    PASSWORD2_FORM.classList.add('d-none');
+    PASSWORD3_FORM.classList.remove('d-none');
+    REGISTRO_FORM.classList.add('d-none');
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'FEASVERSE - Recuperar contraseña';
+}
+
+function showLogIn() {
+    // Se oculta el formulario para iniciar sesión y paso 1, 2
+    LOGIN_FORM.classList.remove('d-none');
+    REGISTRO_FORM.classList.add('d-none');
     // Se muestra el formulario de recuperación de contraseña (paso 3).
-    FORGOT_PASSWORD_STEP_THREE_FORM.classList.remove('d-none');
+    FORGOT_PASSWORD_STEP_THREE_FORM.classList.add('d-none');
     // Se establece el título del contenido principal.
     MAIN_TITLE.textContent = 'FEASVERSE - Recuperar contraseña';
 }
