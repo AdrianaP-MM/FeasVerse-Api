29b9c51a589b176c27a0d72becaf563de320b44f
Fernandouu <20190369@ricaldone.edu.sv>
2024-03-20 06:28:31 -0600

    Cambios

--- controlador/publico/configuraciones.js
+++ controlador/publico/configuraciones.js
@@ -1,3 +1,11 @@
+const NOMBRES_INPUT = document.getElementById('nombreInput'),
+    APELLIDOS_INPUT = document.getElementById('apellidosInput'),
+    DUI_INPUT = document.getElementById('duiInput'),
+    TEL_INPUT = document.getElementById('telefonoInput'),
+    CORREO_INPUT = document.getElementById('correoInput'),
+    FECHAN_INPUT = document.getElementById('fechanInput'),
+    CONTRA_INPUT = document.getElementById('contraInput');
+
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     // *Llamada a la función para mostrar el encabezado y pie del documento.
@@ -18,3 +26,49 @@ function showPedidos()
 {
     location.href = '/vistas/publico/tuspedidos.html';
 }
+
+const returnBack = async () => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Seguro qué quieres regresar?', 'Los datos modificados no serán guardados');
+    if (RESPONSE.isConfirmed) {
+        location.href = '/vistas/publico/index.html'
+    }
+}
+
+const addSave = async () => {
+    const btnUpdate = document.getElementById('btnUpdate');
+
+    var textoBoton = btnUpdate.textContent.trim();
+
+    if (textoBoton == 'Guardar') {
+        await sweetAlert(1, 'Se ha actualizado correctamente', true);
+        btnUpdate.textContent = 'Actualizar';
+        // Hacer los campos de entrada de solo lectura después de guardar.
+        makeFieldsReadOnly(true);
+    }
+    else if (textoBoton == 'Actualizar') {
+        // Hacer los campos de entrada editables para actualizar.
+        makeFieldsReadOnly(false);
+        btnUpdate.textContent = 'Guardar';
+    }
+}
+
+function makeFieldsReadOnly(isReadOnly) {
+    NOMBRES_INPUT.readOnly = isReadOnly;
+    APELLIDOS_INPUT.readOnly = isReadOnly;
+    DUI_INPUT.readOnly = isReadOnly;
+    TEL_INPUT.readOnly = isReadOnly;
+    CORREO_INPUT.readOnly = isReadOnly;
+    FECHAN_INPUT.readOnly = isReadOnly;
+    CONTRA_INPUT.readOnly = isReadOnly;
+}
+
+Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+    }, false)
+})
