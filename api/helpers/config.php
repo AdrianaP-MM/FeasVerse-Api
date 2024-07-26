84bfd7ab6f875555608190b957adcc7711f6798a
Fernandouu <20190369@ricaldone.edu.sv>
2024-07-26 07:23:13 -0600

    Base conectada a servidor

--- api/helpers/config.php
+++ api/helpers/config.php
@@ -4,8 +4,8 @@ header('Access-Control-Allow-Origin: *');
 // Se establece la zona horaria local para la fecha y hora del servidor.
 date_default_timezone_set('America/El_Salvador');
 // Constantes para establecer las credenciales de conexión con el servidor de bases de datos.
-define('SERVER', 'localhost');
-define('DATABASE', 'db_feasverse');
-define('USERNAME', 'root');
-define('PASSWORD', '');
+define('SERVER', 'mysql8001.site4now.net');
+define('DATABASE', 'db_aab69f_ferxxit');
+define('USERNAME', 'aab69f_ferxxit');
+define('PASSWORD', 'feasverse!1');
 ?>
